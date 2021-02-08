58c778885bc924947ad8c7e895957d4d2d299423
bintang_editya <bintangfarhandika@gmail.com>
2021-02-09 02:00:46 +0700

    dbcon

--- config/db.config.js
+++ config/db.config.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 // create here mysql connection
 
 const dbConn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'workonit_db'
+    host: 'db4free.net',
+    user: 'workonit',
+    password: 'workonit',
+    database: 'workonit'
 });
 
 dbConn.connect(function(error){
