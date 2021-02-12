0d7b190d0f8b9cb67b21f0df8a1a43d3a6fa7097
bintang_editya <bintangfarhandika@gmail.com>
2021-02-13 04:19:15 +0700

    fixing 6

--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const taskController = require('../controllers/task.controller');
 
 router.get('/userId/:id_user/bookId/:id_book',taskController.getTaskByIdBookIdUser);
-router.get('detail/:id_task',taskController.getDetailTask)
+router.get('/detail/:id_task',taskController.getDetailTask)
 router.post('/new', taskController.createTask);
 router.delete('/:id_task',taskController.deleteTask)
 router.post('/check',taskController.updateCheck)
